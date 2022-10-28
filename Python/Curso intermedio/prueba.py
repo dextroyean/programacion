e4f615111e5551c13c68406d5e72ec963056d623
Dextroyean <xhavier89@hotmail.com>
2022-10-28 00:37:21 -0500

    i for i en range if

---
+++ Python/Curso intermedio/prueba.py
@@ -0,0 +1,14 @@
+def main():
+    lista = []
+    for i in range (1,10):
+        if i % 3 != 0:
+            lista.append(i**2)
+    
+    print(lista)
+
+#esta condicion es igual a la anterior
+#lista = [ i**2 for i in range(1, 10) if i % 3 != 0]
+
+
+if __name__ == '__main__':
+     main()
